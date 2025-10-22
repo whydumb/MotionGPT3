30bdd1079d3233e485229eef6bbb549edb09d306
BoifZ <57630737+BoifZ@users.noreply.github.com>
2025-10-22 11:58:20 +0800

    Update download_pretrained_motiongpt3_model.sh

--- prepare/download_pretrained_motiongpt3_model.sh
+++ prepare/download_pretrained_motiongpt3_model.sh
@@ -2,5 +2,7 @@ mkdir -p checkpoints/
 cd checkpoints/
 echo -e "The pretrained model motiongpt3.ckpt will stored in the 'checkpoints' folder\n"
 # motiongpt3.ckpt
-gdown "https://drive.google.com/file/d/1cA4uQGAfZ3JlRb5laF3lec8ntF4TJ7rM/view?usp=drive_link"
+gdown "https://drive.google.com/file/d/1Wvx5PGJjVKPRvjcl8firChw1UVjUj36l/view?usp=drive_link"
+
+
 echo -e "Downloading done!"
