32e049bcc93e24c14ad72ac43313ac77b91d76c7
BoifZ <57630737+BoifZ@users.noreply.github.com>
2025-10-27 13:35:16 +0800

    Update load_state_dict

--- demo.py
+++ demo.py
@@ -138,7 +138,7 @@ def main():
         logger.info("Loading checkpoints from {}".format(cfg.TEST.CHECKPOINTS))
         state_dict = torch.load(cfg.TEST.CHECKPOINTS,
                                 map_location="cpu")["state_dict"]
-        msg = model.load_state_dict(state_dict, strict=False)
+        model.load_state_dict(state_dict, strict=False)
     else:
         logger.warning(
             "No checkpoints provided, using random initialized model")
